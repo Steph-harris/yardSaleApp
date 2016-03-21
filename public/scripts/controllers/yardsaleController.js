06147c2c2aa3203da459e95a7ce270210cc7f1f7
steph-harris <steph.harris21@gmail.com>
2016-03-21 17:11:30 -0400

    added controllers file to client side scripts folder for angular directives, added show hide ng functionality

---
+++ public/scripts/controllers/yardsaleController.js
@@ -0,0 +1,16 @@
+app.controller('yardsaleController', function($scope){
+
+//form to log in(send this info to server)
+
+//allow user to sign out
+
+//show username w/ available funds and items bought
+
+//form to post an item
+
+//form to comment on an item
+
+//get items for sale, add them to array to be looped
+
+//call get item function on page load
+});
